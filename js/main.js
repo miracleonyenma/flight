a7352a0912b9360f52ae9d1d61a0b58e2e1834a9
miracleonyenma <miracleficient@gmail.com>
2019-11-28 20:49:39 +0100

    data display and selection

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const flightLoc = document.querySelector("#flight-location");
 const flightDest = document.querySelector("#flight-destination");
+var listEl = document.querySelectorAll(".flights li");
 // const dataList = document.querySelectorAll(".flights");
 
 //helper function for attributes
@@ -10,10 +11,20 @@ const setAttributes = (el, attr) => {
     };
 }
 
+const insertListVal = (targetEl)=>{
+    for(let i = 0; i < listEl.length; i++){
+        listEl[i].addEventListener("click", (e)=>{
+            const optionalval = e.target.innerText;
+            // console.log(targetEl);
+            targetEl.value = optionalval;
+        })
+    }
+}
 
 const populateList = (data, element) => {
-    console.log("populate list called");
+    // console.log("populate list called");
     const dataList = element.target.parentNode.querySelector(".flights");
+    const inputField = element.target.parentNode.querySelector("input");
     
     //while dataList has an element.. delete it
     while(dataList.firstChild){
@@ -24,13 +35,15 @@ const populateList = (data, element) => {
         li.innerText = data[i].name;
         setAttributes(li, {value : data[i].name});
         dataList.appendChild(li);
-    }
 
+        listEl = dataList.querySelectorAll("li");
+    };
+    insertListVal(inputField);
 }
 
 const getData = (e)=>{
     var val = e.target.value; 
-    console.log(val);
+    // console.log(val);
 
     fetch(`https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text?text=${val}`, {
         "method": "GET",
@@ -44,7 +57,7 @@ const getData = (e)=>{
         return res.json();
     })
     .then( data => {
-        console.log(data);
+        // console.log(data);
         populateList(data, e);
     });
 }
@@ -55,3 +68,5 @@ const getData = (e)=>{
 
 flightLoc.addEventListener("input", getData);
 flightDest.addEventListener("input", getData);
+
+
